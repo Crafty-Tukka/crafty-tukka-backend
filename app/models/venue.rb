5090b83436d965f6e075278fbe128de269d6f30d
jwsandeman <jw.sandeman@gmail.com>
2022-07-29 21:54:15 +1000

    updated venue controller to include picture. added get picture url method to venue model

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -7,4 +7,8 @@ class Venue < ApplicationRecord
     has_one_attached :picture
     has_one :position
     accepts_nested_attributes_for :position
+
+    def get_picture_url
+        url_for(self.picture)
+    end
 end
