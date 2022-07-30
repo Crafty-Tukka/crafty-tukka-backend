a962cb7b8e60f75ccb31c402311c48f64404781e
simon-tanna <gcab022109@coderacademy.edu.au>
2022-07-30 15:53:02 +1000

    code implemented to now get the image url for venues

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,8 +1,8 @@
 require "active_support/core_ext/integer/time"
 
-# Rails.application.routes.default_url_options = {
-#   host: 'http://localhost:5000'
-# }
+Rails.application.routes.default_url_options = {
+  host: 'http://localhost:5000'
+}
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
