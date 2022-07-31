eb05be79a3d53289dbb2a778af74d700ad0b20e6
simon-tanna <gcab022109@coderacademy.edu.au>
2022-07-31 15:51:39 +1000

    added extra validations in venue controller for required sign up fields

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -2,8 +2,12 @@ class Venue < ApplicationRecord
     has_secure_password
     has_many :events
     has_many :trucks, through: :events
-    validates :name, presence: true, uniqueness: true
+    validates :name, presence: true, uniqueness: true, length: { minimum: 5 }
     validates :email, presence: true, uniqueness: true
+    validates :description, presence: true, uniqueness: true, length: { minimum: 20 }
+    validates :mobile, presence: true, uniqueness: true
+    validates_numericality_of :mobile
+    validates :address, presence: true, uniqueness: true
     has_one_attached :picture
 
     def picture_url
